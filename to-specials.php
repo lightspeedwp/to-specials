10f72ed88c1d11e982d675715799597658c0d39e
Jacques <jacques@lsdev.biz>
2018-03-19 15:46:21 +0200

    Removed API Class

--- to-specials.php
+++ to-specials.php
@@ -22,62 +22,6 @@ define('LSX_TO_SPECIALS_CORE',  __FILE__ );
 define('LSX_TO_SPECIALS_URL',  plugin_dir_url( __FILE__ ) );
 define('LSX_TO_SPECIALS_VER',  '1.1.0' );
 
-/**
- * Runs once when the plugin is activated.
- */
-function lsx_to_specials_activate_plugin() {
-    $lsx_to_password = get_option('lsx_api_instance',false);
-    if(false === $lsx_to_password){
-    	update_option('lsx_api_instance',LSX_API_Manager::generatePassword());
-    }
-}
-register_activation_hook( __FILE__, 'lsx_to_specials_activate_plugin' );
-
-/* ======================= The API Classes ========================= */
-if(!class_exists('LSX_API_Manager')){
-	require_once('classes/class-lsx-api-manager.php');
-}
-
-/** 
- *	Grabs the email and api key from the LSX Search Settings.
- */ 
-function lsx_to_specials_options_pages_filter($pages){
-	$pages[] = 'lsx-to-settings';
-	return $pages;
-}
-add_filter('lsx_api_manager_options_pages','lsx_to_specials_options_pages_filter',10,1);
-
-function lsx_to_specials_api_admin_init(){
-	$options = get_option('_lsx-to_settings',false);
-	$data = array('api_key'=>'','email'=>'');
-
-	if(false !== $options && isset($options['api'])){
-		if(isset($options['api']['to-specials_api_key']) && '' !== $options['api']['to-specials_api_key']){
-			$data['api_key'] = $options['api']['to-specials_api_key'];
-		}
-		if(isset($options['api']['to-specials_email']) && '' !== $options['api']['to-specials_email']){
-			$data['email'] = $options['api']['to-specials_email'];
-		}		
-	}
-
-	$instance = get_option( 'lsx_api_instance', false );
-	if(false === $instance){
-		$instance = LSX_API_Manager::generatePassword();
-	}
-
-	$api_array = array(
-		'product_id'	=>		'TO Specials',
-		'version'		=>		'1.1.0',
-		'instance'		=>		$instance,
-		'email'			=>		$data['email'],
-		'api_key'		=>		$data['api_key'],
-		'file'			=>		'to-specials.php',
-		'documentation' =>		'tour-operator-special-offers'
-	);
-	
-	$lsx_to_api_manager = new LSX_API_Manager($api_array);
-}
-add_action('admin_init','lsx_to_specials_api_admin_init');
 
 /* ======================= Below is the Plugin Class init ========================= */
 
