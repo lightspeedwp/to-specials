9348397ae03dcf3540068b3718d706d404e0d6df
Warwick Booth <warwick@lsdev.biz>
2019-10-22 13:53:59 +0200

    Enabled the sorting of the gallery field

--- includes/metaboxes/config-special.php
+++ includes/metaboxes/config-special.php
@@ -161,11 +161,13 @@ $metabox['fields'][] = array(
 );
 
 $metabox['fields'][] = array(
-	'id'         => 'gallery',
-	'name'       => esc_html__( 'Gallery', 'to-specials' ),
-	'type'       => 'image',
-	'repeatable' => true,
-	'show_size'  => false,
+	'id'                  => 'gallery',
+	'name'                => '',
+	'type'                => 'image',
+	'repeatable'          => true,
+	'show_size'           => false,
+	'sortable'            => true,
+	'string-repeat-field' => esc_html__( 'Add new image', 'tour-operator' ),
 );
 
 if ( class_exists( 'Envira_Gallery' ) ) {
