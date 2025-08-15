e38a89033d782a36b684be4b126fdfb1f20a85c2
Warwick <krugazul@gmail.com>
2025-08-15 10:23:18 +0200

    Adding in the post field support

--- classes/class-to-specials-admin.php
+++ classes/class-to-specials-admin.php
@@ -36,6 +36,7 @@ class LSX_TO_Specials_Admin extends LSX_TO_Specials {
 		add_filter( 'lsx_get_taxonomies_configs', array( $this, 'taxonomy_config' ), 10, 1 );
 		add_action( 'cmb2_admin_init', array( $this, 'register_cmb2_fields' ) );
 
+		add_filter( 'lsx_to_post_custom_fields', array( $this, 'custom_fields' ) );
 		add_filter( 'lsx_to_destination_custom_fields', array( $this, 'custom_fields' ) );
 		add_filter( 'lsx_to_tour_custom_fields', array( $this, 'custom_fields' ) );
 		add_filter( 'lsx_to_accommodation_custom_fields', array( $this, 'custom_fields' ) );
