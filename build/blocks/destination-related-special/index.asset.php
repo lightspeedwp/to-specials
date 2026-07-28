93fb9d8c130cb175f893501367c6bda85883b639
krugazul <krugazul@gmail.com>
2026-07-28 23:02:03 +0200

    Update grid column count for accommodation, destination, and tour related special blocks to enhance layout flexibility

--- build/blocks/destination-related-special/index.asset.php
+++ build/blocks/destination-related-special/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '88f00e39da351afd0ee9');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '1bd3f5b480cbdc724670');
